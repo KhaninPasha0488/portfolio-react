c05d6fd66ee18149ac47562eeaefb7930ee0d3c3
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-03-30 12:26:45 +0300

    1-init

---
+++ src/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Footer.module.css'
+const Footer = () => {
+    return (
+        <div className={s.footer}>
+<h2>Pavel Khanin</h2>
+        </div>
+    );
+};
+
+export default Footer;
